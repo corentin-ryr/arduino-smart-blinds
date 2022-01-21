480ed800fe54983ea21a3076747d29c022c210ef
Corentin Royer <corentin.35035@gmail.com>
2022-01-21 10:15:13 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Frontend web application to control an arduino
+This project aims at created a frontent web page that communicates with an arduino via a remote serial terminal (based on https://github.com/bricoleau/WiFiTerm). The arduino controls the blinds in the house.
+
+The goal is to have an web interface that is synced with the current state of the blinds (open/close for every blind). The arduino can also be controlled by regular swithes in the house.
+
+## Dependencies
+* https://github.com/bricoleau/WiFiTerm
+* https://github.com/Links2004/arduinoWebSockets
---
+++ esp32_web_serveur6_css/esp32_web_serveur6_css.ino
@@ -0,0 +1,137 @@
+/*********
+  Rui Santos
+  Complete project details at http://randomnerdtutorials.com
+*********/
+
+// Import required libraries
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#else
+#include <ESP8266WiFi.h>
+//  #include <ESPAsyncTCP.h>
+#endif
+//#include <ESPAsyncWebServer.h>
+
+#include "WiFiTerm.h"
+
+// Replace with your network credentials
+const char* ssid = "TP-Link_1C96";
+const char* password = "10502167";
+
+// Set web server port number to 80
+ESP8266WebServer server(80);
+
+String buttonNames[] = {"cuisine", "salon"};
+bool buttonStates[] = {true, false};
+
+
+/**********
+    setup
+ ***********/
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("\nConnecting to WIFI");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print('.');
+  }
+  Serial.println("connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  Serial.println("webServer started");
+
+  term.begin(server);
+  term.link(Serial);
+  Serial.println("term started");
+}
+
+
+
+/**********
+   programme
+ ***********/
+
+void loop() {
+
+  server.handleClient();
+  term.handleClient();
+
+  if (term.available())
+  {
+    String message = term.readString();
+
+    if (String(message[0]) == "1") { //Asking for all buttons with states and names
+      String states = allStates();
+      term.println("1 " + states);
+    }
+    else if(String(message[0]) == "2") { //Update for one button state (command is "2 btnName btnstate")
+      setBtnState(message);
+      term.println("2 ok");
+    }
+  }
+
+
+}
+/*******************************************
+   fin prog principal
+ ****************************************/
+
+String allStates() {
+
+  String states = "[";
+  for (byte i = 0; i < (sizeof(buttonNames) / sizeof(buttonNames[0])); i++) {
+    // do something with myValues[i]
+    if (i > 0) { states += ",";}
+    states += "{'name': '" + buttonNames[i] + "',  'state': " + String(buttonStates[i]) + "}";
+  }
+  states += "]";
+  return states;
+}
+
+void setBtnState(String message) {
+  
+  char buf[message.length() + 1];
+  strcpy(buf, message.c_str());
+  //char *p = buf;
+  char * pch = strtok (buf, " ");
+
+  String btnName;
+  bool btnState;
+  int i = 0;
+  while (pch != NULL)
+  {
+    //printf("%s\n",pch);
+    if(i==1) {
+      btnName = pch;
+    }
+    if(i==2) {
+      btnState= (strcmp(pch, "true") == 0);
+    }
+    
+    pch = strtok (NULL, " ");
+    i++;
+  }
+
+  //Set the button state with its name
+  Serial.println(" ");
+  for (byte i = 0; i < (sizeof(buttonNames) / sizeof(buttonNames[0])); i++) {
+    // do something with myValues[i]
+    if (buttonNames[i] == btnName) { 
+      buttonStates[i] = btnState;
+      break;
+    }
+  }
+}
+
+
+/* On update of one of the switches
+*/
+void onUpdate(String btnName, String btnState) {
+  term.println("3 {'name':" + btnName + ", 'state':true}");
+}
